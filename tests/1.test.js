95991f2d12fe4baf759934a002647fb984780634
Gastonm <gastonnietoarte@gmail.com>
2023-12-11 17:41:22 -0300

    Exercise 1 finished

---
+++ tests/1.test.js
@@ -0,0 +1,7 @@
+const toJadenCase = require('../exercises/1.js');  // Ajusta la ruta según la ubicación de tu archivo ejercicio.js
+
+test('Convierte una cadena a Jaden Case correctamente', () => {
+  const texto = "hola, esta es una cadena de ejemplo";
+  const resultado = toJadenCase.call(texto); // Usar call para pasar 'texto' como 'this'
+  expect(resultado).toBe("Hola, Esta Es Una Cadena De Ejemplo");
+});
